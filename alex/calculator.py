48b4320f11079e835dc5071d0a3c92772c24e3c3
Sanya Zaicev <sanyazaicev325@gmail.com>
2022-11-06 11:59:01 +0300

    added log and while in calculator

--- alex/calculator.py
+++ alex/calculator.py
@@ -1,21 +1,25 @@
-import math
-znak = input("введите знак(если корень пишите 'корень'):")
-if znak == "корень":
-    a = int(input())
-else:
-    a = int(input())
-    b = int(input())
-if znak == "-":
-    print(f"разность чисел {a} и {b} = {a - b}")
-if znak == "+":
-    print(f"сумма чисел {a} и {b} = {a + b}")
-if znak == "//":
-    print(f"целочисленное деление {a} на {b} = {a // b}")
-if znak == "%":
-    print(f"остаток от деления {a} на {b} = {a - b}")
-if znak == "*":
-    print(f"сумма чисел {a} и {b} = {a - b}")
-if znak == "**":
-    print(f"сумма чисел {a} и {b} = {a - b}")
-if znak == "корень":
-    print(math.sqrt(int(input())))
+while True:
+    import math
+
+    znak = input("введите знак(если корень пишите 'корень', если логарифм пишите: 'логарифм'):")
+    if znak == "корень":
+        a = int(input())
+    else:
+        a = int(input())
+        b = int(input())
+    if znak == "-":
+        print(f"разность чисел {a} и {b} = {a - b}")
+    if znak == "+":
+        print(f"сумма чисел {a} и {b} = {a + b}")
+    if znak == "//":
+        print(f"целочисленное деление {a} на {b} = {a // b}")
+    if znak == "%":
+        print(f"остаток от деления {a} на {b} = {a - b}")
+    if znak == "*":
+        print(f"сумма чисел {a} и {b} = {a - b}")
+    if znak == "**":
+        print(f"сумма чисел {a} и {b} = {a - b}")
+    if znak == "корень":
+        print(math.sqrt(int(input())))
+    if znak == "логарифм":
+        print(math.log(b, a))
