0394102f3e3951dc06ebba4c97470bd3275f7130
Sanya Zaicev <sanyazaicev325@gmail.com>
2022-11-06 12:22:21 +0300

    я сделал доп. задачи но мой калькулятор очень забывчивый и забывает прошлую историю после того как его запускают снова

--- alex/calculator.py
+++ alex/calculator.py
@@ -1,5 +1,7 @@
+import math
+
+calc = open("calculator_history.txt", "w")
 while True:
-    import math
 
     znak = input("введите знак(если корень пишите 'корень', если логарифм пишите: 'логарифм'):")
     if znak == "корень":
@@ -9,17 +11,33 @@ while True:
         b = int(input())
     if znak == "-":
         print(f"разность чисел {a} и {b} = {a - b}")
+        calc.write(f"разность чисел {a} и {b} = {a - b}" + "\n")
+
     if znak == "+":
         print(f"сумма чисел {a} и {b} = {a + b}")
+        calc.write(f"сумма чисел {a} и {b} = {a + b}" + "\n")
+
     if znak == "//":
         print(f"целочисленное деление {a} на {b} = {a // b}")
+        calc.write(f"целочисленное деление {a} на {b} = {a // b}" + "\n")
+
     if znak == "%":
         print(f"остаток от деления {a} на {b} = {a - b}")
+        calc.write(f"остаток от деления {a} на {b} = {a - b}" + "\n")
+
     if znak == "*":
         print(f"сумма чисел {a} и {b} = {a - b}")
+        calc.write(f"сумма чисел {a} и {b} = {a - b}" + "\n")
+
     if znak == "**":
         print(f"сумма чисел {a} и {b} = {a - b}")
+        calc.write(f"сумма чисел {a} и {b} = {a - b}" + "\n")
+
     if znak == "корень":
-        print(math.sqrt(int(input())))
+        print(f"корень из {a} = {math.sqrt(a)}")
+        calc.write(f"корень из {a} = {math.sqrt(a)}" + "\n")
+
     if znak == "логарифм":
-        print(math.log(b, a))
+        print(f"я не знаю как правильно сформулировать так что вот вам ваш логарифм: {math.log(b, a)}")
+        calc.write(f"я не знаю как правильно сформулировать так что вот вам ваш логарифм: {math.log(b, a)}")
+
